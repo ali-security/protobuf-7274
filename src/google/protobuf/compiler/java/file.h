protocolbuffers/protobuf

internal change
PiperOrigin-RevId: 805992219

src/google/protobuf/compiler/java/file.h
@@ -71,6 +71,7 @@ class FileGenerator {
 
  private:
   void GenerateDescriptorInitializationCodeForImmutable(io::Printer* printer);
+  void GenerateDescriptorGetCollectedExtensionRegistry(io::Printer* printer);
 
   bool ShouldIncludeDependency(const FileDescriptor* descriptor,
                                bool immutable_api_);

ali-security/protobuf-7274

src/google/protobuf/compiler/java/file.h
@@ -71,6 +71,7 @@ class FileGenerator {
 
  private:
   void GenerateDescriptorInitializationCodeForImmutable(io::Printer* printer);
+  void GenerateGetCollectedExtensionRegistry(io::Printer* printer);
 
   bool ShouldIncludeDependency(const FileDescriptor* descriptor,
                                bool immutable_api_);