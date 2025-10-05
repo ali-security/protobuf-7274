4a181ca16897d87501b0a2579185775375e79f77
Lev Pachmanov <lev.p@sealsecurity.io>
2025-10-05 19:19:05 +0300

    fix

--- kokoro/release/python/windows/build_single_artifact.bat
+++ kokoro/release/python/windows/build_single_artifact.bat
@@ -4,31 +4,31 @@ if %PYTHON%==C:\python36_32bit set generator=Visual Studio 17 2022
 if %PYTHON%==C:\python36_32bit set vcplatform=Win32
 
 if %PYTHON%==C:\python36 set generator=Visual Studio 17 2022
-if %PYTHON%==C:\python36 set vcplatform=Win64
+if %PYTHON%==C:\python36 set vcplatform=x64
 
 if %PYTHON%==C:\python37_32bit set generator=Visual Studio 17 2022
 if %PYTHON%==C:\python37_32bit set vcplatform=Win32
 
 if %PYTHON%==C:\python37 set generator=Visual Studio 17 2022
-if %PYTHON%==C:\python37 set vcplatform=Win64
+if %PYTHON%==C:\python37 set vcplatform=x64
 
 if %PYTHON%==C:\python38_32bit set generator=Visual Studio 17 2022
 if %PYTHON%==C:\python38_32bit set vcplatform=Win32
 
 if %PYTHON%==C:\python38 set generator=Visual Studio 17 2022
-if %PYTHON%==C:\python38 set vcplatform=Win64
+if %PYTHON%==C:\python38 set vcplatform=x64
 
 if %PYTHON%==C:\python39_32bit set generator=Visual Studio 17 2022
 if %PYTHON%==C:\python39_32bit set vcplatform=Win32
 
 if %PYTHON%==C:\python39 set generator=Visual Studio 17 2022
-if %PYTHON%==C:\python39 set vcplatform=Win64
+if %PYTHON%==C:\python39 set vcplatform=x64
 
 if %PYTHON%==C:\python310_32bit set generator=Visual Studio 17 2022
 if %PYTHON%==C:\python310_32bit set vcplatform=Win32
 
 if %PYTHON%==C:\python310 set generator=Visual Studio 17 2022
-if %PYTHON%==C:\python310 set vcplatform=Win64
+if %PYTHON%==C:\python310 set vcplatform=x64
 
 REM Prepend newly installed Python to the PATH of this build (this cannot be
 REM done from inside the powershell script as it would require to restart
